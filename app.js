1834893388801684f22905e917dd0e7d58de4a58
Marialaina <pmarialaina@gmail.com>
2021-01-13 16:01:55 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-
+//Adding my html to JavaScript by grabbing the classes or id's
 const usrInput = document.getElementById('input-number');
 const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
+const clearBtn = document.getElementById('btn-clear');
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
+const equalBtn = document.getElementById('btn-equal');
 
 
+/*making the currentResultOutput and currentCalculationOutput =
+to a variable which is result and text. Will the function not work
+if instead of using result and text it would both be result? 
+It probably doesn't matter. It can be named anything*/
 const outputResult = (result, text) => {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
 
-const defaultResult = 0;
-let currentResult = defaultResult;
+//current result is going to be 0.
+let currentResult = 0;
 let logEntries = [];
 
 //GETS INPUT FROM INPUT FIELD
@@ -26,6 +32,11 @@ const  getUserNumberInput = () => {
 const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) => {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
+}
+
+const equal = () => {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
 }
 
 const add = () => {
@@ -55,7 +66,16 @@ const divide = () => {
   currentResult /= enteredNumber;
   createAndWriteOutput('/', initialResult, enteredNumber);
 }
+
+const clear = () => {
+  location.reload();
+}
+
+
+
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
+clearBtn.addEventListener('click', clear);
+equalBtn.addEventListener('click', equal);
